8ffc5c3f34cd12bd48bdbd217f1e6ad7d1c1e056
xjl <xjl@jusfound.com>
2016-09-29 15:06:03 +0800

    add start args

--- src/com/xujl/util/proxyip/server/ProxyServer.java
+++ src/com/xujl/util/proxyip/server/ProxyServer.java
@@ -5,30 +5,62 @@ import org.restlet.Server;
 import org.restlet.data.Protocol;
 import org.restlet.ext.jaxrs.JaxRsApplication;
 
+import com.xujl.util.proxyip.core.ProxyIPool;
+
 public class ProxyServer {
 
-    public static void main(String[] args) throws Exception {
-        // create Component (as ever for Restlet)
-        Component comp = new Component();
-        int port = 8182;
-//        if(args.length )
+	public static void main(String[] args) throws Exception {
+		int port = 8182;
+		try {
+
+			if (args.length == 1) {
+				if ("-recover".equals(args[0]))
+					ProxyIPool.load();
+				else
+					port = Integer.parseInt(args[0]);
+			}
+			if (args.length == 2) {
+				if ("-f".equals(args[0])) {
+					ProxyIPool.initIPs(args[1]);
+				} else {
+					port = Integer.parseInt(args[0]);
+					ProxyIPool.load();
+				}
+			}
+			if (args.length == 3) {
+				port = Integer.parseInt(args[0]);
+				ProxyIPool.initIPs(args[2]);
+			}
+
+		} catch (Exception e) {
+			System.out.println("java -jar proxyService-0.5.1-SNAPSHOT.jar [port] [-f] [/home/ips.txt]");
+
+			System.out.println("java -jar proxyService-0.5.1-SNAPSHOT.jar [port] [-recover");
+			return;
+		}
+		System.out.println("java -jar proxyService-0.5.1-SNAPSHOT.jar [port] [-f] [/home/ips.txt]");
+
+		System.out.println("java -jar proxyService-0.5.1-SNAPSHOT.jar [port] [-recover]");
+	
+		Component comp = new Component();
+		// if(args.length )
 		Server server = comp.getServers().add(Protocol.HTTP, port);
 
-        // create JAX-RS runtime environment
-        JaxRsApplication application = new JaxRsApplication(comp.getContext());
+		// create JAX-RS runtime environment
+		JaxRsApplication application = new JaxRsApplication(comp.getContext());
 
-        // attach ApplicationConfig
-        application.add(new ProxyApplication());
+		// attach ApplicationConfig
+		application.add(new ProxyApplication());
 
-        // Attach the application to the component and start it
-        comp.getDefaultHost().attach(application);
-        comp.start();
-        
-        System.out.println("Server started on port " + server.getPort());
-        System.out.println("Press key to stop server");
-        System.in.read();
-        System.out.println("Stopping server");
-        comp.stop();
-        System.out.println("Server stopped");
-    }
+		// Attach the application to the component and start it
+		comp.getDefaultHost().attach(application);
+		comp.start();
+
+		System.out.println("Server started on port " + server.getPort());
+		System.out.println("Press key to stop server");
+		System.in.read();
+		System.out.println("Stopping server");
+		comp.stop();
+		System.out.println("Server stopped");
+	}
 }
